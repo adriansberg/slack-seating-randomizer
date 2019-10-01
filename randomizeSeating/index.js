f96237c6f10c2a10a6952a67b624bc1d74e3f0a0
Adrian Santana Berg <adrian_berg96@hotmail.com>
2019-10-01 17:17:56 +0200

    Add function randomizeSeating
    This function responds to a message action on a slack message.
    The bot will randomize the list of users that reacted with a valid
    reaction, and reply to the message with that list. If there is no valid
    reactions the bot will send an ephemeral message to the user invoking
    the message action.

---
+++ randomizeSeating/index.js
@@ -0,0 +1,113 @@
+const { WebClient } = require('@slack/web-api');
+const querystring = require('querystring');
+const _ = require('lodash');
+
+const FOOD_EMOJIS = [
+  'avocado',
+  'hamburger',
+  'pizza',
+  'hotdog',
+  'taco',
+  'burrito',
+  'sushi'
+];
+
+const client = new WebClient(process.env.TOKEN);
+
+async function getReactions(channel, timestamp) {
+  const response = await client.reactions.get({
+    channel: channel,
+    timestamp: timestamp
+  });
+
+  const { reactions } = response.message;
+
+  return reactions ? reactions : [];
+}
+
+async function sendMessage(channelId, threadTimestamp, users) {
+  return await client.chat.postMessage({
+    channel: channelId,
+    text: _.shuffle(users).join('\n'),
+    as_user: false,
+    thread_ts: threadTimestamp,
+    username: 'Sitteplasser'
+  });
+}
+
+async function sendEphemeral(channelId, user) {
+  return await client.chat.postEphemeral({
+    channel: channelId,
+    attachments: [
+      {
+        fallback: 'Could not randomize seating order. No valid reaction found.',
+        color: '#f00',
+        pretext: '_Could not randomize seating order_',
+        title: 'No valid reaction found',
+        text: 'See the list of valid reactions below',
+        fields: [
+          {
+            title: 'Valid reactions',
+            value: FOOD_EMOJIS.join(', '),
+            short: false
+          }
+        ]
+      }
+    ],
+    as_user: false,
+    user: user
+  });
+}
+
+async function getUserList() {
+  const response = await client.users.list();
+  const { members } = response;
+  return members ? members : [];
+}
+
+async function mapIdsToNames(ids) {
+  const userList = await getUserList();
+
+  const usersIdAndName = userList.reduce((obj, user) => {
+    obj[user.id] = user.profile.display_name;
+    return obj;
+  }, []);
+
+  const userNames = ids.reduce((arr, userId) => {
+    arr.push(usersIdAndName[userId]);
+    return arr;
+  }, []);
+
+  return userNames;
+}
+
+module.exports = async function(context, req) {
+  if (req.headers['content-type'] !== 'application/x-www-form-urlencoded') {
+    context.res = { status: 400 };
+    return;
+  }
+
+  const parsedBody = querystring.parse(req.body);
+  const parsedPayload = JSON.parse(parsedBody.payload);
+
+  if (_.isNil(parsedPayload)) {
+    context.res = { status: 400 };
+    return;
+  }
+
+  const { channel, message } = parsedPayload;
+
+  const reactions = await getReactions(channel.id, message.ts);
+  context.log(reactions);
+
+  const reaction = reactions.find(reaction =>
+    FOOD_EMOJIS.includes(reaction.name)
+  );
+
+  if (reaction) {
+    const userNames = await mapIdsToNames(reaction.users);
+    await sendMessage(channel.id, message.ts, userNames);
+  } else {
+    await sendEphemeral(channel.id, message.user);
+  }
+};
